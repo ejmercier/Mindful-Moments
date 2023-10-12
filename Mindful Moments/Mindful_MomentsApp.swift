f97cb4d0e3e81d663cc71362a2f16d89b6a0a14f
ejmercier <ejmercier15@gmail.com>
2023-10-12 10:34:15 -0700

    Initial Commit

---
+++ Mindful Moments/Mindful_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindful_MomentsApp.swift
+//  Mindful Moments
+//
+//  Created by user247884 on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Mindful_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
