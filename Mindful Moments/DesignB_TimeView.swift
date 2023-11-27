9509b0692328f0c2141fe92f61e3da150d72ec0c
catherinepham <cathy.02pham@gmail.com>
2023-11-27 17:38:23 -0500

    add second time picker to indicate time range

--- Mindful Moments/DesignB_TimeView.swift
+++ Mindful Moments/DesignB_TimeView.swift
@@ -8,31 +8,43 @@
 import SwiftUI
 
 struct DesignB_TimeView: View {
-    @ObservedObject private var selectedTime = Time()
+    @ObservedObject private var minTime = Time()
+    @ObservedObject private var maxTime = Time()
     @Environment(UserInputModel.self) private var userInput: UserInputModel
     @State private var selectedFeeling: String = ""
     
     var body: some View {
         VStack {
-            Text("How much time do you have?").font(.system(size: 32))
+            Text("How much time do you have?")
+                .font(.system(size: 32))
+                .padding()
             //TODO: fix time picker to be able to add time to userinput
-            TimePicker(countdownInterval: $selectedTime.timeInterval).padding(.bottom)
-
-           //button to the next page (main screen)
-            NavigationLink("Find Recommendations") {
-                MainScreenView(time: selectedTime.timeInterval)
-                    .environment(userInput)
-                    .navigationTitle("Mindful Moments")
-            }
-            .buttonStyle(.bordered)
-            .font(.title)
-            .padding()
+            Text("Minimum:")
+                .font(.title2)
+            TimePicker(countdownInterval: $minTime.timeInterval).padding(.bottom)
+                .frame(height: 150)
+            
+            Text("Maximum:")
+                .font(.title2)
+                .padding(.top)
+            TimePicker(countdownInterval: $maxTime.timeInterval).padding(.bottom)
+                .frame(height: 150)
         }
-        .padding(.top)
+        .padding()
+        
+        //button to the next page (main screen)
+         NavigationLink("Find Recommendations") {
+             MainScreenView(minTime: minTime.timeInterval, maxTime: maxTime.timeInterval)
+                 .environment(userInput)
+                 .navigationTitle("Mindful Moments")
+         }
+         .buttonStyle(.bordered)
+         .padding()
+
     }
 }
 
 #Preview {
-    MainScreenView(time: 0)
+    MainScreenView(minTime: 0, maxTime: 5)
 }
 
